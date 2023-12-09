a08dd196419f32ac2af71c337e9510d473784419
w3degens <irmant.g@gmail.com>
2023-12-09 21:59:09 +0330

    fix error in_app

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import App from 'next/app';
 
 import '../src/styles/custom.css';
 import Head from 'next/head';
+import { Card } from '@mui/material';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,7 +13,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Your Website</title>
       </Head>
-      <body>
+      <Card>
         <main>
           <header class="main-header">
             <div class="logo">Your Logo</div>
@@ -39,7 +40,7 @@ function MyApp({ Component, pageProps }) {
             </ul>
           </nav>
         </footer>
-      </body>
+      </Card>
     </>
   );
 }
